0059703de6f49fe66cbf6f18a991687fa7651142
Ira Tyshchenko <tyshchenk20@gmail.com>
2022-01-04 18:47:47 +0200

    modified structure, created migration

--- Identity/DisasterTrackerApp.Identity/Models/ForceTokenRefreshModel.cs
+++
@@ -1,7 +0,0 @@
-namespace DisasterTrackerApp.Identity.Controllers;
-
-    public record ForceTokenRefreshModel
-    {
-        public IReadOnlyList<string> Results;
-        public string AccessToken;
-    }
