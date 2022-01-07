35780bc0292b42a2f956e418e34e5b9060abdbc2
Vadym Mulish <lamborghini43346@gmail.com>
2022-01-07 23:11:06 +0200

    Extract Google OAuth registration endpoint to separate controller

--- src/Tracking/DisasterTrackerApp.WebApi/Controllers/RegistrationController.cs
+++ src/Tracking/DisasterTrackerApp.WebApi/Controllers/RegistrationController.cs
@@ -44,30 +44,6 @@ public class UsersController : ControllerBase
         return Ok(googleRegistrationUrl);
     }
     
-    [ApiExplorerSettings(IgnoreApi = true)]
-    [HttpGet("register")]
-    [ProducesResponseType(typeof(ApiResponse<RegistrationResponse>), (int) HttpStatusCode.OK)]
-    [ProducesResponseType(typeof(ApiError), (int) HttpStatusCode.BadRequest)]
-    public async Task<IActionResult> Register([FromQuery] string code, [FromQuery] string scope, CancellationToken cancellationToken)
-    {
-        var userId = await _registrationService.RegisterUser(code, cancellationToken);
-
-        if (userId == default)
-        {
-            var errorMessage = "Something bad happened. " +
-                               "Unable to register user because of network failure or user is already registered";
-            
-            return BadRequest(new ApiError(ErrorCode.InternalError, errorMessage));
-        }
-        
-        var response = new RegistrationResponse
-        {
-            UserId = userId.ToString()
-        };
-
-        return Ok(new ApiResponse<RegistrationResponse>(response));
-    }
-    
     [HttpGet("login")]
     [ProducesResponseType(typeof(ApiResponse<LoginResponse>), (int) HttpStatusCode.OK)]
     [ProducesResponseType(typeof(ApiError), (int) HttpStatusCode.BadRequest)]
